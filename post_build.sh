462749a5e4b33860296e387dedd843f737c95203
krjj <krjamdade@gmail.com>
2018-02-01 09:36:42 +0530

    14

---
+++ post_build.sh
@@ -0,0 +1,4 @@
+mkdir -p $HOME/.sigma && echo "Directory created [$HOME/.sigma]"
+cp -R ./sigma $HOME/.sigma && echo "Executable copied to .sigma"
+cd $HOME/.sigma && touch sigmalog.log 
+(crontab -l ; echo "@reboot cd \$HOME/.sigma && ./sigma") | crontab - && echo 'Program will launch at system startup automatically.'
